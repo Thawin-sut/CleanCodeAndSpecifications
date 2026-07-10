1a9dcec7ced52587917d4f0103779834f75c3022
Thawin-sut <thawin.sut@ku.th>
2026-07-10 19:14:59 +0700

    Add files via upload

--- src/lab/BankAccount.java
+++ src/lab/BankAccount.java
@@ -13,7 +13,14 @@ public class BankAccount {
      */
     public void withdraw(int amount) {
         // TODO: amount <= 0  -> throw IllegalArgumentException  (input ภายนอก = exception)
+
+        if(amount <= 0)
+            throw new IllegalArgumentException("No enough money");
+        assert amount <= balance;
+        balance -= amount;
+
         // TODO: assert amount <= balance                        (เงื่อนไขภายใน = assert)
         // TODO: balance ลดลงเท่ากับ amount
+        
     }
 }
